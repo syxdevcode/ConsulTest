4a3f834d96c4e2158fedf56a3ca74a35cbb17692
ShiyxProjectCode <syxtest01@gmail.com>
2018-06-21 10:01:28 +0800

    添加服务预警项目

---
+++ NoticeService/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace NoticeService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+
+        public ValuesController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [HttpPost("/notice")]
+        //[HttpGet("/notice")]
+        public IActionResult Notice()
+        {
+            var bytes = new byte[10240];
+            var i = Request.Body.ReadAsync(bytes, 0, bytes.Length);
+            var content = System.Text.Encoding.UTF8.GetString(bytes).Trim('\0');
+
+            EmailSettings settings = new EmailSettings()
+            {
+                SmtpServer = Configuration["Email:SmtpServer"],
+                SmtpPort = Convert.ToInt32(Configuration["Email:SmtpPort"]),
+                AuthAccount = Configuration["Email:AuthAccount"],
+                AuthPassword = Configuration["Email:AuthPassword"],
+                ToWho = Configuration["Email:ToWho"],
+                ToAccount = Configuration["Email:ToAccount"],
+                FromWho = Configuration["Email:FromWho"],
+                FromAccount = Configuration["Email:FromAccount"],
+                Subject = Configuration["Email:Subject"]
+            };
+
+            EmailHelper.SendHealthEmail(settings, content);
+
+            return Ok();
+        }
+    }
+}
---
+++ NoticeService/EmailHelper.cs
@@ -0,0 +1,58 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoticeService
+{
+    public class EmailHelper
+    {
+        public static void SendHealthEmail(EmailSettings settings, string content)
+        {
+            try
+            {
+                dynamic list = JsonConvert.DeserializeObject(content);
+                if (list != null && list.Count > 0)
+                {
+                    var emailBody = new StringBuilder("健康检查故障:\r\n");
+                    foreach (var noticy in list)
+                    {
+                        emailBody.AppendLine($"--------------------------------------");
+                        emailBody.AppendLine($"Node:{noticy.Node}");
+                        emailBody.AppendLine($"Service ID:{noticy.ServiceID}");
+                        emailBody.AppendLine($"Service Name:{noticy.ServiceName}");
+                        emailBody.AppendLine($"Check ID:{noticy.CheckID}");
+                        emailBody.AppendLine($"Check Name:{noticy.Name}");
+                        emailBody.AppendLine($"Check Status:{noticy.Status}");
+                        emailBody.AppendLine($"Check Output:{noticy.Output}");
+                        emailBody.AppendLine($"--------------------------------------");
+                    }
+
+                    var message = new MimeMessage();
+                    message.From.Add(new MailboxAddress(settings.FromWho, settings.FromAccount));
+                    message.To.Add(new MailboxAddress(settings.ToWho, settings.ToAccount));
+
+                    message.Subject = settings.Subject;
+                    message.Body = new TextPart("plain") { Text = emailBody.ToString() };
+                    using (var client = new SmtpClient())
+                    {
+                        client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                        client.Connect(settings.SmtpServer, settings.SmtpPort, false);
+                        client.AuthenticationMechanisms.Remove("XOAUTH2");
+                        client.Authenticate(settings.AuthAccount, settings.AuthPassword);
+                        client.Send(message);
+                        client.Disconnect(true);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
